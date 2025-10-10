db548510bd5a61f6c32a0897bca4298e6e7fc85a
GoncharovMax <gonchmaxalex@gmail.com>
2025-10-10 16:51:00 +0800

    Создание класса продукт

---
+++ src/main/java/by/tms/tmsc35p2/configuration/Product.java
@@ -0,0 +1,50 @@
+package by.tms.tmsc35p2.configuration;
+
+public class Product {
+
+    private int id;
+    private String title;
+    private String description;
+    private double price;
+    private byte[] image;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
